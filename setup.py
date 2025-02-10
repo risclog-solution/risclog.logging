b776244ff6dfcd5456608e690b9d144ed7e3e3a0
Sebastian Wehrmann <sebastian@risclog.com>
2025-02-10 08:21:33 +0100

    Preparing release 1.3.0

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     namespace_packages=['risclog'],
     package_dir={'': 'src'},
     url='https://github.com/risclog-solution/risclog.logging',
-    version='1.3.0.dev',
+    version='1.3.0',
     zip_safe=False,
 )
