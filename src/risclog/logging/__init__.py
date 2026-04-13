6128e7581661c6fad731da25dc5bdbc8e6b03551
Jakob Horbank <jakob@risclog.com>
2026-04-13 11:54:59 +0200

    Fix package exports

--- src/risclog/logging/__init__.py
+++ src/risclog/logging/__init__.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from risclog.logging.decorators import log_decorator as log_decorator
+from risclog.logging.decorators import log_decorator
 from risclog.logging.log import HybridLogger, get_logger as old_get_logger
 from risclog.logging.log import getLogger as logger
 
@@ -8,3 +8,5 @@ getLogger: Callable[[str], HybridLogger] = logger
 
 # DeprecationWarning: 'get_logger' is obsolete and will be removed from version 2.1.0. Please use 'getLogger' instead.
 get_logger: Callable[[str], HybridLogger] = old_get_logger
+
+__all__ = ["getLogger", "get_logger", "log_decorator"]
